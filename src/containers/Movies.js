963785638214b34ce3847ea229359145ae131c88
GUYOT Julie <guyotjulie@MacBook-Pro-de-GUYOT.local>
2021-05-02 17:06:56 +0200

    delete pagination

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,14 +1,10 @@
 import React, {useState} from "react"
 import Sort from '../components/Sort'
 import MovieCard from '../components/MovieCard'
-import Pagination from '../components/Pagination'
 
 const Movies = ({ movies }) => {
 const [data, setData] = useState(movies);
 const [category, setCategory] = useState("")
-// const [offset, setOffset] = useState(0);
-// const [perPage] = useState(4);
-// const [pageCount, setPageCount] = useState(0)
 
 // we make a copy of the array and delete the movie wanted
 const handleClickTrash = (index) => {
@@ -18,11 +14,6 @@ const handleClickTrash = (index) => {
     const newCategory = [...category];
 
   };
-
-// const handlePageClick = (data) => {
-//     const selectedPage = data.selected;
-//     setOffset(selectedPage + 1)
-// };
 
 return (
     <div className="container">
@@ -45,7 +36,7 @@ return (
                 }
             })}
         </div>
-        {/* <Pagination handlePageClick={handlePageClick} pageCount={pageCount} /> */}
+        {/* <Pagination /> */}
     </div>
   );
 };
